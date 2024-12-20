40b1636670115975cd34c8c952cb8256ae00606c
Prakash Sahu <89311090+prakash-dev-code@users.noreply.github.com>
2024-12-20 17:30:29 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-
-# Exit script on any error
-set -e
-
-# Install Node.js 18 if not installed
-if ! command -v node &> /dev/null || [[ "$(node -v)" != v18* ]]; then
-    echo "Installing Node.js 18..."
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-else
-    echo "Node.js 18 is already installed."
-fi
-
-# Install dependencies and build the React application
 echo "Installing dependencies and building the application..."
 cd /var/www/react-gym-website
+cp -R ./ /var/www/react-gym-website
 npm install
 npm run build
+
