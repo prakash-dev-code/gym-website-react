6813dd12ee5940413ff04f769fd73fba913f52bc
Prakash Sahu <89311090+prakash-dev-code@users.noreply.github.com>
2024-12-20 17:16:13 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies and building the application..."
 cd /var/www/gym-website-react
-cp -R /path/to/source/* .
+cp -R ./ /var/www/react-gym-website
 npm install
 npm run build
